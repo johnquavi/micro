eb6e455094063aac2efe867423c555ea2ba0b729
johnquavi <122548093+johnquavi@users.noreply.github.com>
2023-01-20 11:22:26 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let rps = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showString("hi ")
+})
+input.onButtonPressed(Button.AB, function () {
+    rps = randint(1, 6)
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("bye ")
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Happy)
+})
